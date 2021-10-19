ce5938c978c4fe5263a29022cca39bc8f13ef695
VictorWong123 <91344431+VictorWong123@users.noreply.github.com>
2021-10-19 18:57:34 -0400

    Added stuff

---
+++ app_5.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template 
+
+app = flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/very_cool")
+def very_cool():
+    pass
